69e535dc9c2609c20ad2a8af8af0179f85f5504e
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-06-04 21:15:04 +0430

    remove delete folder operation with parameter in url code of index file

--- index.php
+++ index.php
@@ -2,19 +2,6 @@
 
 include "bootstrap/init.php";
 
-// Delete operation
-if (isset($_GET["delete_folder_id"])) {
-    
-    $id = sanitizeInput($_GET["delete_folder_id"]);
-    
-    // Execute delete operation
-    $result = removeFolder($id);
-
-    if (!($result)) {
-        error("Your folder is not exist!");
-    }
-}
-
 $folders = getFolder(1);
 
 include "tpl/tpl-index.php";
