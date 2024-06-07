8ec90141b95a14399817c6796e5ff6d5077eb3aa
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-06-07 20:39:57 +0430

    delete

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include "bootstrap/init.php";
-
-$dbPassword = getRecords("password", "users", "email", "arshiabd1388@gmail.com");
-$dbEmail = getRecords("email", "users", "email", "arshiabd1388@gmail.coms");
-
-print_r($dbPassword);
-print_r($dbEmail);
