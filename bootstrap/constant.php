98d6fdb390a337b9a330efa06798f26607d283f7
Arshia Moharrary <167417699+Arshia-Moharrary@users.noreply.github.com>
2024-06-12 18:29:41 +0330

    Update constant.php

--- bootstrap/constant.php
+++ bootstrap/constant.php
@@ -1,6 +1,6 @@
 <?php
 
-define("PROJECT_NAME", "arshia"); /* The value of this constant must be equal to the name of the root directory */
+define("PROJECT_NAME", "ArshiaToDo"); /* The value of this constant must be equal to the name of the root directory */
 
 define("WEB_TITLE", "ArshiaToDo Task manager | Manage your tasks");
 define("BASE_URL", "http://" . $_SERVER['SERVER_NAME'] . "/" . PROJECT_NAME . "/");
